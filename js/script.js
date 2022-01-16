90ea09175304da8f253c68daa7c2b59c41919685
saharabanu <saharabanungn@gmail.com>
2022-01-16 15:47:59 +0600

    headerr added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn= document.querySelector('#menu-btn');
+menuBtn.onclick=()=>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
