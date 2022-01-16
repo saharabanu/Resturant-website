22abe48d377cc9150c9295c6af4e6df2fcb83912
saharabanu <saharabanungn@gmail.com>
2022-01-16 23:17:59 +0600

    food preview added

--- js/script.js
+++ js/script.js
@@ -38,3 +38,25 @@ var swiper = new Swiper(".food-slider", {
         },
       },
   });
+  let previewContainer = document.querySelector('.food-preview-container');
+  let previewBox = document.querySelectorAll('.food-preview');
+  document.querySelectorAll('.food .slide').forEach(food=>{
+      food.onclick=()=>{
+          previewContainer.style.display='flex';
+          let name = food.getAttribute('data-name');
+          previewBox.forEach(preview=>{
+              let target= preview.getAttribute('data-target');
+              if(name == target){
+                  preview.classList.add('active')
+              }
+          })
+      }
+  });
+  previewContainer.querySelector('#close-preview').onclick = ()=>{
+      previewContainer.style.display='none';
+      previewBox.forEach(close=>{
+        
+        close.classList.remove('active')
+        
+    })
+  }
