d046b8194d903b59fd8aec32c4fb1c408ff2fe72
saharabanu <saharabanungn@gmail.com>
2022-01-24 14:46:35 +0600

    blogs added

--- js/script.js
+++ js/script.js
@@ -72,3 +72,29 @@ var swiper = new Swiper(".food-slider", {
    
     
   });
+  var swiper = new Swiper(".blogs-slider", {
+    grabCursor:true,
+    loop:true,
+    autoHeight:true,
+    spaceBetween:20,
+    centeredSlides:true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+   
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+          
+        },
+        700: {
+          slidesPerView: 2,
+          
+        },
+        1000: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
